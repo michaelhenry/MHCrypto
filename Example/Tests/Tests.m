7b5f812afc2f145b63f169340351654b6833f403
Michael Henry Pantaleon <me@iamkel.net>
2017-02-22 18:55:37 +0900

    update example

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -18,7 +18,7 @@ describe(@"Checking HMAC", ^{
         
         NSString *secretKey = @"hello";
         NSString *time = @"123456";
-        NSString *hmacSha256 = [[secretKey mh_sha256DigestWithMessage:time]mh_toHexString];
+        NSString *hmacSha256 = [[secretKey mh_hmacSha256DigestWithMessage:time]mh_toHexString];
         expect(hmacSha256).to
             .equal(@"8814a5904d59cb8b41da8073a2c4b95c20818935afc3a0ad48dbc4ffb476d8ac");
         
@@ -35,19 +35,19 @@ describe(@"Checking HMAC", ^{
         NSString *regionName = @"us-east-1";
         NSString *serviceName = @"iam";
         
-        NSData *kDate = [[NSString stringWithFormat:@"AWS4%@",key]mh_sha256DigestWithMessage:dateStamp];
+        NSData *kDate = [[NSString stringWithFormat:@"AWS4%@",key]mh_hmacSha256DigestWithMessage:dateStamp];
         NSLog(@"%@",[kDate mh_toHexString]);
         expect([kDate mh_toHexString]).to.equal(@"969fbb94feb542b71ede6f87fe4d5fa29c789342b0f407474670f0c2489e0a0d");
         
-        NSData *kRegion = [kDate mh_sha256DigestWithMessage:regionName];
+        NSData *kRegion = [kDate mh_hmacSha256DigestWithMessage:regionName];
         NSLog(@"%@",[kRegion mh_toHexString]);
         expect([kRegion mh_toHexString]).to.equal(@"69daa0209cd9c5ff5c8ced464a696fd4252e981430b10e3d3fd8e2f197d7a70c");
         
-        NSData *kService = [kRegion mh_sha256DigestWithMessage:serviceName];
+        NSData *kService = [kRegion mh_hmacSha256DigestWithMessage:serviceName];
         NSLog(@"%@",[kService mh_toHexString]);
         expect([kService mh_toHexString]).to.equal(@"f72cfd46f26bc4643f06a11eabb6c0ba18780c19a8da0c31ace671265e3c87fa");
         
-        NSData *kSigning = [kService mh_sha256DigestWithMessage:@"aws4_request"];
+        NSData *kSigning = [kService mh_hmacSha256DigestWithMessage:@"aws4_request"];
         NSLog(@"%@",[kSigning mh_toHexString]);
         expect([kSigning mh_toHexString]).to.equal(@"f4780e2d9f65fa895f9c67b32ce1baf0b0d8a43505a000a1a9e090d414db404d");
         
